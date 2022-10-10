3f1d66b69b2bd178a943f192deb8a58f23740442
unknown <nwayne@dev-10.com>
2022-10-10 12:15:05 -0500

    updated authcontroller, appconfig, securityconfig

--- server/src/main/java/horror/controller/AuthController.java
+++ server/src/main/java/horror/controller/AuthController.java
@@ -1,11 +1,13 @@
 package horror.controller;
 
+import horror.security.JwtConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,29 +17,30 @@ import java.util.Map;
 
 @RestController
 public class AuthController {
-    // The `AuthenticationManager` interface defines a single method `authenticate()`
-    // that processes an Authentication request.
-    private final AuthenticationManager authenticationManager;
 
-    public AuthController(AuthenticationManager authenticationManager) {
+    private final AuthenticationManager authenticationManager;
+    private final JwtConverter converter;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtConverter converter) {
         this.authenticationManager = authenticationManager;
+        this.converter = converter;
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<?> authenticate(@RequestBody Map<String, String> credentials) {
-        // The `UsernamePasswordAuthenticationToken` class is an `Authentication` implementation
-        // that is designed for simple presentation of a username and password.
+    public ResponseEntity<Map<String, String>> authenticate(@RequestBody Map<String, String> credentials) {
+
         UsernamePasswordAuthenticationToken authToken =
                 new UsernamePasswordAuthenticationToken(credentials.get("username"), credentials.get("password"));
 
         try {
-            // The `Authentication` interface Represents the token for an authentication request
-            // or for an authenticated principal once the request has been processed by the //
-            // `AuthenticationManager.authenticate(Authentication)` method.
             Authentication authentication = authenticationManager.authenticate(authToken);
 
             if (authentication.isAuthenticated()) {
+                String jwtToken = converter.getTokenFromUser((User) authentication.getPrincipal());
+
                 HashMap<String, String> map = new HashMap<>();
+                map.put("jwt_token", jwtToken);
+
                 return new ResponseEntity<>(map, HttpStatus.OK);
             }
 
@@ -46,5 +49,16 @@ public class AuthController {
         }
 
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity<Map<String, String>> refreshToken(UsernamePasswordAuthenticationToken principal) {
+        User user = new User(principal.getName(), principal.getName(), principal.getAuthorities());
+        String jwtToken = converter.getTokenFromUser(user);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("jwt_token", jwtToken);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
--- server/src/main/java/horror/security/SecurityConfig.java
+++ server/src/main/java/horror/security/SecurityConfig.java
@@ -15,6 +15,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // This method allows configuring web based security for specific http requests.
+
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // we're not using HTML forms in our app
@@ -28,16 +35,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // the order of the antMatchers() method calls is important
         // as they're evaluated in the order that they're added
         http.authorizeRequests()
-                // new...
                 .antMatchers("/authenticate").permitAll()
-                .antMatchers(HttpMethod.GET, "/order").permitAll()
-                .antMatchers(HttpMethod.GET, "/sighting", "/sighting/*").permitAll()
-                .antMatchers(HttpMethod.POST, "/sighting").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.PUT, "/sighting/*").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/sighting/*").hasRole("ADMIN")
+                // new...
+                .antMatchers("/refresh_token").authenticated()
+                .antMatchers(HttpMethod.GET,
+                        "/order").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/sighting", "/sighting/*").permitAll()
+                .antMatchers(HttpMethod.POST,
+                        "/sighting").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT,
+                        "/sighting/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE,
+                        "/sighting/*").hasAnyRole("ADMIN")
                 // if we get to this point, let's deny all requests
                 .antMatchers("/**").denyAll()
                 .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(), converter))
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
