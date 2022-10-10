2dfd4f4650428989b3d4b1b2ad882b4591d1d3a0
gabrielc42 <clark.gabe97@gmail.com>
2022-10-10 11:27:34 -0500

    added folder for diagrams and databases

---
+++ server/src/main/java/horror/controller/AuthController.java
@@ -0,0 +1,22 @@
+package horror.controller;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ValidationException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+}
--- server/src/main/java/horror/controller/MovieController.java
+++ server/src/main/java/horror/controller/MovieController.java
@@ -1,4 +1,7 @@
 package horror.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MovieController {
 }
