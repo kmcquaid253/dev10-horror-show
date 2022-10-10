a103208d4e206090b48e9c6c55a9a7f84e9be51f
Megan Lane <meganlane7458@gmail.com>
2022-10-10 13:22:46 -0500

    cross origins

--- server/src/main/java/horror/controller/MovieController.java
+++ server/src/main/java/horror/controller/MovieController.java
@@ -1,7 +1,9 @@
 package horror.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class MovieController {
 }
