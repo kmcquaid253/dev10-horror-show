c209461337b2b5888b18a34a02136064d5ff5d62
unknown <nwayne@dev-10.com>
2022-10-10 11:47:22 -0500

    updated securityconfig, appconfig, authcontroller, jwtconverter

--- server/src/main/java/horror/security/SecurityConfig.java
+++ server/src/main/java/horror/security/SecurityConfig.java
@@ -1,10 +1,16 @@
 package horror.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -34,5 +40,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userBuilder = User.withUsername("user")
+                .password("user").passwordEncoder(password -> encoder.encode(password))
+                .roles("USER");
+
+        var adminBuilder = User.withUsername("admin")
+                .password("admin").passwordEncoder(password -> encoder.encode(password))
+                .roles("ADMIN");
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder)
+                .withUser(adminBuilder);
     }
 }
