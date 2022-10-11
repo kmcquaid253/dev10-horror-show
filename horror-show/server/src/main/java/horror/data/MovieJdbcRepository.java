b295e85c907c2dd9771577dbe5dc3a14620016ed
gabrielc42 <clark.gabe97@gmail.com>
2022-10-11 14:14:23 -0500

    repository interfaces implemented

--- horror-show/server/src/main/java/horror/data/MovieJdbcRepository.java
+++ horror-show/server/src/main/java/horror/data/MovieJdbcRepository.java
@@ -1,7 +1,25 @@
 package horror.data;
 
+import horror.models.Movie;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class MovieJdbcRepository implements MovieRepository {
+    @Override
+    public List<Movie> findAll() throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Movie findById(int id) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Movie findFriendById(int id) throws DataAccessException {
+        return null;
+    }
 }
--- horror-show/server/src/main/java/horror/data/MovieRepository.java
+++ horror-show/server/src/main/java/horror/data/MovieRepository.java
@@ -1,4 +1,12 @@
 package horror.data;
 
+import horror.models.Movie;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
 public interface MovieRepository {
+    List<Movie> findAll() throws DataAccessException;
+    Movie findById(int id) throws DataAccessException;
+    Movie findFriendById(int id) throws DataAccessException;
 }
