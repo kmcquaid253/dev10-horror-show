cf494958e32ad9ec5be8239aa1e4da7ebb971f70
unknown <nwayne@dev-10.com>
2022-10-11 08:59:38 -0500

    Work on Model classes

--- horror-show/server/src/main/java/horror/models/Movie.java
+++ horror-show/server/src/main/java/horror/models/Movie.java
@@ -1,4 +1,96 @@
 package horror.models;
 
+import java.time.LocalDate;
+
 public class Movie {
+
+    private int movieId;
+
+    private String title;
+
+    private String runtime;
+
+    private String rating;
+
+    private LocalDate releaseDate;
+
+    private int scoreNum;
+
+    private int actorId;
+
+    private int directorId;
+
+    private int subgenreId;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getScoreNum() {
+        return scoreNum;
+    }
+
+    public void setScoreNum(int scoreNum) {
+        this.scoreNum = scoreNum;
+    }
+
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
+
+    public int getDirectorId() {
+        return directorId;
+    }
+
+    public void setDirectorId(int directorId) {
+        this.directorId = directorId;
+    }
+
+    public int getSubgenreId() {
+        return subgenreId;
+    }
+
+    public void setSubgenreId(int subgenreId) {
+        this.subgenreId = subgenreId;
+    }
 }
--- horror-show/server/src/main/java/horror/models/Review.java
+++ horror-show/server/src/main/java/horror/models/Review.java
@@ -1,4 +1,44 @@
 package horror.models;
 
 public class Review {
+
+    private int reviewId;
+
+    private String userReview;
+
+    private int appUserId;
+
+    private int movieId;
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getUserReview() {
+        return userReview;
+    }
+
+    public void setUserReview(String userReview) {
+        this.userReview = userReview;
+    }
+
+    public int getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(int appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
 }
