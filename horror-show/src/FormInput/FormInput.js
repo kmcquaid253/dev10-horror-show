e708fa30a7a2d2b6976a6da1f32037ec8dae8d06
Megan Lane <meganlane7458@gmail.com>
2022-10-11 10:56:37 -0500

    form input and register componenent progress

---
+++ horror-show/src/FormInput/FormInput.js
@@ -0,0 +1,15 @@
+function FormInput(props) {
+    return (
+        <div>
+            <label className="form-label" htmlFor={props.identifier}>{props.labelText}</label>
+            <input className="form-control"
+            type={props.inputType}
+            id={props.identifier}
+            name={props.identifier}
+            defaultValue={props.currentValue}
+            onChange={props.onChangeHandler}/>
+        </div>
+    );
+}
+
+export default FormInput;
