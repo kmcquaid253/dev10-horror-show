29f01bf549a54193dfd1684752e92e063bb45e9a
unknown <nwayne@dev-10.com>
2022-10-11 11:31:36 -0500

    Work on Login/Back-end

--- horror-show/server/src/main/java/horror/controller/AuthController.java
+++ horror-show/server/src/main/java/horror/controller/AuthController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080"})
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class AuthController {
 
     // The `AuthenticationManager` interface defines a single method `authenticate()`
