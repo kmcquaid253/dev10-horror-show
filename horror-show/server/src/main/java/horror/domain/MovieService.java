7448e75f9eebdc2b5512962b708e560a2446a0e2
gabrielc42 <clark.gabe97@gmail.com>
2022-10-11 15:24:19 -0500

    movie controller initializaed, movie service throwing unsupported access exception

--- horror-show/server/src/main/java/horror/domain/MovieService.java
+++ horror-show/server/src/main/java/horror/domain/MovieService.java
@@ -1,7 +1,18 @@
 package horror.domain;
 
+import horror.models.Movie;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService {
+    public List<Movie> findAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Movie findById(int movieId) {
+        throw new UnsupportedOperationException();
+    }
 }
