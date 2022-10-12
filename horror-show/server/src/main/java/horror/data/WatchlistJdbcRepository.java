198159c75933aff2a6228ca2aabe517a33eb14a2
unknown <nwayne@dev-10.com>
2022-10-12 11:49:44 -0500

    added create method for watchlistJdbcRepo

--- horror-show/server/src/main/java/horror/data/WatchlistJdbcRepository.java
+++ horror-show/server/src/main/java/horror/data/WatchlistJdbcRepository.java
@@ -3,12 +3,24 @@ package horror.data;
 import horror.models.Review;
 import horror.models.Watchlist;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
 public class WatchlistJdbcRepository implements WatchlistRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public WatchlistJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Watchlist> findAll() throws DataAccessException {
         return null;
@@ -16,7 +28,22 @@ public class WatchlistJdbcRepository implements WatchlistRepository{
 
     @Override
     public Watchlist create(Watchlist watchlist) throws DataAccessException {
-        return null;
+
+        final String sql = "insert into watchlist_movie (movieId, app_user_id) values "
+                + "(?,?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, watchlist.getMovie().getMovieId());
+            ps.setInt(2, watchlist.getAppUser().getAppUserId());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+        return watchlist;
     }
 
     @Override
