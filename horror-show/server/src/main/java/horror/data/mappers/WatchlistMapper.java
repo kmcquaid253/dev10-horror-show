e31647ce0dd54456c8288b217a46880fee7229b6
unknown <nwayne@dev-10.com>
2022-10-12 10:44:43 -0500

    Added Watchlost model and mapper

--- horror-show/server/src/main/java/horror/data/mappers/WatchlistMapper.java
+++ horror-show/server/src/main/java/horror/data/mappers/WatchlistMapper.java
@@ -1,4 +1,24 @@
 package horror.data.mappers;
 
-public class WatchlistMapper {
+import horror.models.Watchlist;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WatchlistMapper implements RowMapper<Watchlist> {
+
+    @Override
+    public Watchlist mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        Watchlist watchlist = new Watchlist();
+
+        MovieMapper movieMapper = new MovieMapper();
+        watchlist.setMovie(movieMapper.mapRow(resultSet, i));
+
+//        AppUserMapper appUserMapper = new AppUserMapper();
+//        watchlist.setAppUser(appUserMapper.mapRow(resultSet, i));
+
+        return watchlist;
+    }
 }
--- horror-show/server/src/main/java/horror/models/Watchlist.java
+++ horror-show/server/src/main/java/horror/models/Watchlist.java
@@ -1,4 +1,24 @@
 package horror.models;
 
 public class Watchlist {
+
+    private Movie movie;
+
+    private AppUser appUser;
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
 }
