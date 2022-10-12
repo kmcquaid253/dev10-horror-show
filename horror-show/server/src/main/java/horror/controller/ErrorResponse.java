4bcfda9953d26faafb667975e5b8188fadf27e0c
unknown <nwayne@dev-10.com>
2022-10-12 08:56:58 -0500

    Added ErrorResponse Class

---
+++ horror-show/server/src/main/java/horror/controller/ErrorResponse.java
@@ -0,0 +1,19 @@
+package horror.controller;
+
+import horror.domain.Result;
+import horror.domain.ResultType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ErrorResponse {
+
+    public static <T> ResponseEntity<Object> build(Result<T> result) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        if (result.getType() == null || result.getType() == ResultType.INVALID) {
+            status = HttpStatus.BAD_REQUEST;
+        } else if (result.getType() == ResultType.NOT_FOUND) {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(result.getMessages(), status);
+    }
+}
