27e28acc9d990399335be4741635990c3eec7e3a
unknown <nwayne@dev-10.com>
2022-10-12 12:00:38 -0500

    set jdbcrepo testing for watchlist

--- horror-show/server/src/test/java/horror/data/ReviewJdbcTemplateRepositoryTests.java
+++ horror-show/server/src/test/java/horror/data/ReviewJdbcTemplateRepositoryTests.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 public class ReviewJdbcTemplateRepositoryTests {
 
     static final int NEXT_ID = 6;
