0db5964288a3d8ff908cf1d4ce890815b896aec3
unknown <nwayne@dev-10.com>
2022-10-13 08:54:22 -0500

    Changes to backend Friend classes

--- horror-show/server/src/main/java/horror/controller/FriendController.java
+++ horror-show/server/src/main/java/horror/controller/FriendController.java
@@ -1,6 +1,5 @@
 package horror.controller;
 
-import horror.domain.FriendService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +9,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/friend")
 public class FriendController {
 
-    private final FriendService service;
-
-    public FriendController(FriendService service) {
-        this.service = service;
-    }
 }
