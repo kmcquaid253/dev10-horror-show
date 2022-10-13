aa5007a6a82eeca7c3f8cacc7a34c4dcb298daa2
unknown <nwayne@dev-10.com>
2022-10-13 17:12:34 -0500

    Work on watchlist controller

--- horror-show/server/src/main/java/horror/controller/WatchlistController.java
+++ horror-show/server/src/main/java/horror/controller/WatchlistController.java
@@ -1,9 +1,13 @@
 package horror.controller;
 
+import horror.domain.Result;
 import horror.domain.WatchlistService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import horror.models.Watchlist;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:8080"})
@@ -15,5 +19,19 @@ public class WatchlistController {
 
     public WatchlistController(WatchlistService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public List<Watchlist> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody Watchlist watchlist) {
+        Result<Watchlist> result = service.create(watchlist);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
     }
 }
