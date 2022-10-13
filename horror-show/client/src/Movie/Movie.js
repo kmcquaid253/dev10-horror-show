bbd49642c30d1b5005f5aba29dbbf2b5e4fde8de
gabrielc42 <clark.gabe97@gmail.com>
2022-10-13 14:16:35 -0500

    neon text

--- horror-show/client/src/Movie/Movie.js
+++ horror-show/client/src/Movie/Movie.js
@@ -33,6 +33,7 @@ function Movie({ title, poster_path, release_date, overview }) {
     return (
         <div className="card">
             <div className="card-body">
+
                 <img className="card-img" src={getPosterUrl(poster_path)} alt={title + " " + overview}></img>
                 <div hidden={!show}>
                         <div className="modal-background" onClick={handleModalClose}>
