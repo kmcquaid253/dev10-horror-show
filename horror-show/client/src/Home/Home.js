57e88e2e5b8ff682ba87f2e7fe75e5236bf3544d
Megan Lane <meganlane7458@gmail.com>
2022-10-13 10:46:30 -0500

    styling and working on home page

--- horror-show/client/src/Home/Home.js
+++ horror-show/client/src/Home/Home.js
@@ -20,6 +20,7 @@ function Home() {
     }, []);
 
 
+
     return (
         
         <main>
--- horror-show/client/src/Movie/Movie.js
+++ horror-show/client/src/Movie/Movie.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 function Movie({ title, poster_path, release_date, overview }) {
+
+    const movieUrl = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US";
 
     const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
@@ -8,14 +12,19 @@ function Movie({ title, poster_path, release_date, overview }) {
         return `https://www.themoviedb.org/t/p/w220_and_h330_face${posterpath}`; // this link leads to correct movie poster
     } // don't know how to connect the object to this though
 
+
+
     return (
         <div className="card">
             <div className="card-body">
-                <h6>{title}</h6>
-                <h6>Release Date: {release_date}</h6>
+                {/* <h6>{title}</h6>
+                <h6>Release Date: {release_date}</h6> */}
                 {/* <p>{overview}</p> */}
+
                 <img className="card-img" src={getPosterUrl(poster_path)} alt={title + " " + overview}></img>
+
             </div>
+        
         </div>
     )
 }
