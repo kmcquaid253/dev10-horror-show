d57c7ca6a05c43cee90fbe84301ff224ed4e5628
unknown <nwayne@dev-10.com>
2022-10-14 13:07:58 -0500

    continuing Friends.js

--- horror-show/client/src/Friend/Friends.js
+++ horror-show/client/src/Friend/Friends.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 function Friends(){
 
+    const [errors, setErrors] = useState([]);
+    const [friends, setFriends] = useState({});
+
+    function handleSubmit(event) {//take in an event to prevent it from posting
+        event.preventDefault();
+    };
+
     return(
+
         <div className="container">
             <h2>Friends:</h2>
         </div>
