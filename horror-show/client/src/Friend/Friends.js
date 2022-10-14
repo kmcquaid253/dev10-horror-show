389a73caf4dd76849664291569ab4c58e1d6771e
unknown <nwayne@dev-10.com>
2022-10-14 15:02:01 -0500

    display progress on Friends

--- horror-show/client/src/Friend/Friends.js
+++ horror-show/client/src/Friend/Friends.js
@@ -1,20 +1,22 @@
-function Friends({name}){
+import './Friends.css';
+
+function Friends(){
 
     const friendURL = "";
 
     return(
 
-        <div className="container">
-            <h2>Friends:</h2>
+        <div className="heading">
+            <h1 className="heading-header">Friends</h1>
 
-            <div className="card">
-                <div className="card-header">
-                    <h5>{name}</h5>
-                </div>
-                <div className="card-body">
-                    <button onClick={friendURL} className="button">See Reviews</button>
-                </div>
+        <div className="card">
+            <div className="card-header">
+                <h5>Friend Name goes here</h5>
             </div>
+            <div className="card-body">
+                <button onClick={friendURL} className="button">See Reviews</button>
+            </div>
+        </div>
         </div>
     );
 
