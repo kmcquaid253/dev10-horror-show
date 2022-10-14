daee9884a7163a88da2a0df0ef8065243f9dacc8
unknown <nwayne@dev-10.com>
2022-10-14 13:45:58 -0500

    added Friends.css

--- horror-show/client/src/Friend/Friends.js
+++ horror-show/client/src/Friend/Friends.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
-function Friends(){
-
-    const [errors, setErrors] = useState([]);
-    const [friends, setFriends] = useState({});
-
-    function handleSubmit(event) {//take in an event to prevent it from posting
-        event.preventDefault();
-    };
+function Friends({name}){
 
     return(
 
         <div className="container">
             <h2>Friends:</h2>
+
+            <div className="card">
+                <div className="card-header">
+                    <h5>{name}</h5>
+                </div>
+                <div className="card-body">
+                    <button className="button">See Reviews</button>
+                </div>
+            </div>
         </div>
     );
 
