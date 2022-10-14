52a84099cb0dd58b11df93c8a3157bf52bcc86f6
KAREN MCQUAID <kmcquaid253@gmail.com>
2022-10-14 16:42:45 +0100

    updates

--- horror-show/client/src/AddReview/AddReview.js
+++ horror-show/client/src/AddReview/AddReview.js
@@ -35,7 +35,7 @@ function AddReview() {
         event.preventDefault();
 
         //Use fetch to POST to the service
-        fetch("http://localhost:8080/api/review", {
+        fetch("http://localhost:8080/api/review#", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
--- horror-show/client/src/Watchlist/WatchLater.js
+++ horror-show/client/src/Watchlist/WatchLater.js
@@ -1,20 +1,12 @@
-// import React, { useContext } from 'react';
-// import { DataContext } from './DataContext';
+// // import React, { useContext } from 'react';
+// // import { DataContext } from './DataContext';
 
-<<<<<<< HEAD
-// export default WatchLater() {
-
+// export default function WatchLater() {
 //     const {watchLater, setWatchLater} = useContext{DataContext};
+    
 //     return (
 //         <div>
-=======
-export default function WatchLater() {
-    const {watchLater, setWatchLater} = useContext{DataContext};
-    
-    return (
-        <div>
->>>>>>> b1bbe3cc4c6d68165f2a75e0820da65347dc0bd1
 
-//         </div>
-//     );
-// }
+// //         </div>
+// //     );
+// // }
