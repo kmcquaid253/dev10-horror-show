45837c676d61e4ab35eb68f6d580d362aad7b45d
gabrielc42 <clark.gabe97@gmail.com>
2022-10-14 09:59:55 -0500

    database table reorganized

--- horror-show/client/src/Watchlist/WatchLater.js
+++ horror-show/client/src/Watchlist/WatchLater.js
@@ -1,6 +1,12 @@
+import React, { useContext } from 'react';
+import { DataContext } from './DataContext';
+
 export default WatchLater() {
+
+    const {watchLater, setWatchLater} = useContext{DataContext};
     return (
         <div>
+
         </div>
-    )
+    );
 }
