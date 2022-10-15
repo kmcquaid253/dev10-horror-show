53040b7f198f518ba88c2cb6badaba073cc99ae0
Megan Lane <meganlane7458@gmail.com>
2022-10-15 16:04:09 -0500

    for pull

--- horror-show/client/src/Watchlist/WatchLater.js
+++ horror-show/client/src/Watchlist/WatchLater.js
@@ -1,41 +1,6 @@
-// // import React, { useContext } from 'react';
-// // import { DataContext } from './DataContext';
+import React, { useContext } from 'react';
+import { DataContext } from './DataContext';
 
-<<<<<<< HEAD
-// export default function WatchLater() {
-//     const { watchLater, setWatchLater } = useContext(DataContext);
-
-//     return (
-//         <div>
-//             <h1 id="watch-list">Watch List</h1>
-//             <div className="watchLater-movies-cnt">
-//                 {watchLater ?
-//                     watchLater.map((movie) => {
-//                         return (
-//                             <div className="movies-item">
-//                                 <h3 className="movie-title">
-//                                 {movie.title}</h3>
-//                                 {movie.poster_path ? (
-//                                     <img
-//                                         className="movie-poster"
-//                                         alt={`${movie.title} poster`}
-//                                         src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`}
-//                                     />
-//                                 ) : (
-//                                     <div className="noimage">
-//                                         <p>Poster unavailable</p>
-//                                     </div>
-//                                 )}
-//                                 <div className="movie-buttons"></div>
-//                             </div>
-//                         );
-//                     }) 
-//                 : null}
-//             </div>
-//         </div>
-//     );
-// }
-=======
 export default function WatchLater() {
     const { watchLater, setWatchLater, addToWatched } = useContext(DataContext);
 
@@ -94,4 +59,3 @@ export default function WatchLater() {
         </div>
     );
 }
->>>>>>> 6c4457495e02f98be9c3b217e55d5d027fd639ab
