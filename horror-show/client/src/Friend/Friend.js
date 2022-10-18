e44f72780b19860111c966d41ec6180a731a8ba9
unknown <nwayne@dev-10.com>
2022-10-18 13:30:33 -0500

    friends page layout changes

--- horror-show/client/src/Friend/Friend.js
+++ horror-show/client/src/Friend/Friend.js
@@ -2,22 +2,15 @@ import './Friend.css';
 
 function Friend({ friendData }){
 
-    // const friendURL = "http://localhost:8080/friend/" + userB;
-
     return(
-
-        <div className="heading">
-            <h1 className="heading-header">Friends</h1>
 
         <div className="friendCard">
             <div className="card-header">
-                {/* <h5 className='name'>{userA}</h5> */}
-                {/* <h5 className='name'>{userB}</h5> */}
+
                 <h5 className='name'>{friendData.username}</h5>
             </div>
             <div className="card-body">
             </div>
-        </div>
         </div>
     );
 
