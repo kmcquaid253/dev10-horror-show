4e881320bc0472c91dc0a422941f8361f9258bb4
unknown <nwayne@dev-10.com>
2022-10-19 14:07:58 -0500

    changes to testing classes

--- horror-show/server/src/test/java/horror/domain/FriendServiceTests.java
+++ horror-show/server/src/test/java/horror/domain/FriendServiceTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -23,7 +24,6 @@ public class FriendServiceTests {
 
     @Test
     void shouldFindById() {
-
     }
 
     Friend makeFriend() {
